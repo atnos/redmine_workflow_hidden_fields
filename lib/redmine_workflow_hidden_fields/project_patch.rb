216af124c35012fd5a703d8067aa31d013a2d45c
Cire DIA <ciremoussadia@gmail.com>
2022-12-12 11:24:28 +0000

    fixup! Plug model to process

--- lib/redmine_workflow_hidden_fields/project_patch.rb
+++ lib/redmine_workflow_hidden_fields/project_patch.rb
@@ -3,7 +3,7 @@ module RedmineWorkflowHiddenFields
 
 		# Returns list of attributes that are hidden on all statuses of all trackers for +user+ or the current user.
 		def completely_hidden_attribute_names(user=User.current)
-			HiddenAttributeNamesForUser.where(user_id: user.id).pluck(:name).map{ |r| r.split(';')}.flatten.uniq
+			HiddenAttributeNamesForUser.where(user_id: user.id, project_id: id).pluck(:name).map{ |r| r.split(';')}.flatten.uniq
 		end
 	end
 end
